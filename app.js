edea1cd01b054ae438d35331724b5516f87492eb
= <Japan8789@gmail.com>
2016-11-30 20:59:58 -0500

    Dark Sky API switch to jsonp

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var GOOGLE_MAP_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
-var DARK_SKY_API_URL = 'https://api.darksky.net/forecast/';
+var DARK_SKY_API_URL = 'https://api.darksky.net/forecast';
 
 var getLocation = function(search){
         
 };
 
-var getGoogleMapResponse = function(search, callback){
+var getGoogleMapData = function(search, callback){
     var settings ={
         url: GOOGLE_MAP_API_URL,
         data: {
@@ -20,6 +20,7 @@ var getGoogleMapResponse = function(search, callback){
     $.ajax(settings);
 };
 
+
 var log = function(data){
     console.log(data);
     if(data.status === "OK"){
@@ -30,20 +31,29 @@ var log = function(data){
         console.log(data.status);
 };
 
-var getDarkSkyResponse = function(data, callback){
+var getDarkSkyData = function(data){
    if(data.status === "OK"){
         var lat = data.results[0].geometry.location.lat;
         var lng = data.results[0].geometry.location.lng;
-        //get DARKSKYRESPONSE
+        var key = '52048a5f9e5bf2d1771414fe6a221c8e'
+
+        var settings ={
+            url: DARK_SKY_API_URL + `/${key}/${lat},${lng}`,
+            dataType: 'jsonp',
+            type: 'GET',
+            crossDomain: true,
+            success: displayResults
+        };
+
+        $.ajax(settings);
     }
     else{
         displayResults(data.status);
     };
 };
 
-var displayResults = function(data, element){
-    var toDisplay = "The data is : " + data;
-    element.html(toDsiplay);
+var displayResults = function(data){
+    console.log(data);
 }
 
-getGoogleMapResponse("New York City, NY", log);
+getGoogleMapData("Paris", getDarkSkyData);
